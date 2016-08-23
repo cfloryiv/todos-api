38de25d35f870bbea235f7a918d3578acc655856
cfloryiv <cfloryiv@rcn.com>
2016-08-23 12:24:28 -0400

    new apis

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
 var express=require('express');
 var app=express()
 var PORT = process.env.PORT || 3000;
+var todos = [{
+    id: 1,
+    description: "meet mom for lunch",
+    completed: false
+}, {
+    id: 2,
+    description: "goto market",
+    completed: false
+}, {
+    id: 3,
+    description: "drive to work",
+    completed: true
+}];
 
 app.get('/', function (req, res) {
     res.send('Todo API Request');
 });
 
+app.get('/todos', function (req, res) {
+    res.json(todos);
+});
+
+app.get('/todos/:id', function (req, res) {
+    //res.send("ask for todos of id: "+req.params.id);
+    var todoID=parseInt(req.params.id, 10);
+    var foundID;
+    todos.forEach(function (todo) {
+        if (todo.id===todoID) {
+            foundID=todo;
+        }
+    });
+    if (foundID) {
+        res.json(foundID);
+    } else {
+        res.status(404).send();
+    }
+});
 app.listen(PORT, function() {
     console.log('Express listening on port '+PORT);
 
