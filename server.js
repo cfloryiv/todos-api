f5f15c7c60da7721c71cd825600b4859300676fd
cfloryiv <cfloryiv@rcn.com>
2016-08-28 20:45:32 -0400

    added query params

--- server.js
+++ server.js
@@ -16,7 +16,20 @@ app.get('/', function (req, res) {
 });
 
 app.get('/todos', function (req, res) {
-    res.json(todos);
+    var qp = req.query;
+    var filteredTodos=todos;
+
+    if (qp.hasOwnProperty('completed') && (qp.completed === "true")) {
+        filteredTodos=_.where(filteredTodos, {completed: true});
+    } else {
+        if (qp.hasOwnProperty('completed') && (qp.completed === "false")) {
+            filteredTodos=_.where(filteredTodos, {completed: false});
+        } else {
+            filteredTodos=todos;
+        }
+    }
+
+    res.json(filteredTodos);
 });
 
 app.get('/todos/:id', function (req, res) {
