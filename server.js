a944b418a4a393fa948f0318b8e53b68807e02c3
cfloryiv <cfloryiv@rcn.com>
2016-09-18 21:17:41 -0400

    hash and salt password

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.post('/users', function(req, res) {
     var body=_.pick(body, 'email', 'password');
 
     db.user.create(body).then(function(user) {
-      res.json(user.toJSON());
+      res.json(user.toPublicJSON());
     }).error(function(e) {
       res.status(400).json(e);
     });
